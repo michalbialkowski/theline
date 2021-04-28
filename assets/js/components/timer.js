a30d1a608d477223e75168c90cdd97f9453f46ff
Michał Białkowski <michal@bialkowski.net.pl>
2021-04-29 01:43:52 +0200

    Update

---
+++ assets/js/components/timer.js
@@ -0,0 +1,29 @@
+$(window).on('load', function() {
+	let countDownDateString = '2021-10-11T10:11:21';
+	
+	var countDownDate = new Date(countDownDateString).getTime();
+	var x = setInterval(function() {
+		var now = new Date().getTime();
+		var distance = countDownDate - now;
+
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
+
+		if (minutes < 10) {
+			minutes = '0' + minutes;
+		}
+		
+		$('.timer--time--days').text(days);
+		$('.timer--time--hours').text(hours);
+		$('.timer--time--minutes').text(minutes);
+		$('.timer--time--seconds').text(seconds);
+	}, 1000);
+});
+
+console.log('ok');
