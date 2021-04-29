8fe6d5476208886c7ffefeeac6198a9f2d22b650
Michał Białkowski <michal@bialkowski.net.pl>
2021-04-29 13:35:33 +0200

    Update

--- assets/js/components/menu.js
+++ assets/js/components/menu.js
@@ -5,18 +5,22 @@ $('#navbar__desktop .btn__menu').on('click', function() {
 
 let menu_content_slider = '.content--slider';
 
-$('#navbar__desktop .link__inwestycja').on('click', function() {
+$('#navbar__desktop .link__inwestycja').on('click', function(event) {
+	event.preventDefault();
 	$(menu_content_slider).flickity('select', 0);
 });
 
-$('#navbar__desktop .link__przedsprzedaz').on('click', function() {
+$('#navbar__desktop .link__przedsprzedaz').on('click', function(event) {
+	event.preventDefault();
 	$(menu_content_slider).flickity('select', 1);
 });
 
-$('#navbar__desktop .link__apartamenty').on('click', function() {
+$('#navbar__desktop .link__apartamenty').on('click', function(event) {
+	event.preventDefault();
 	$(menu_content_slider).flickity('select', 2);
 });
 
-$('#navbar__desktop .link__kontakt').on('click', function() {
+$('#navbar__desktop .link__kontakt').on('click', function(event) {
+	event.preventDefault();
 	$(menu_content_slider).flickity('select', 3);
 });
